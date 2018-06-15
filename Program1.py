678b5193c2eb3646d25b9375cd3378f6035ad872
Ritika Dhyani <39532567+RitikaDhyani@users.noreply.github.com>
2018-06-16 00:57:35 +0530

    Initial commit

---
+++ Program1.py
@@ -0,0 +1,18 @@
+print("Dictionaries are Key-Value pairs.")
+
+d = {
+    'word':'definition',
+    'word2':'definition2',
+    'word3' : 'definition3'
+}
+
+a = {
+    'a':43,
+    'b':56,
+    'c':89
+}
+
+print("d =",d)
+print("a =",a)
+print("a['b'] =",a['b'])
+print("d['word3'] =",d['word3'])
